ee2942e5cc7a42e13aa25fb75dfc3196e2731e7f
KillerOfTimeSK <adam.adamkluciar@gmail.com>
2024-05-07 21:51:04 +0200

    subfolders should work better with training. Made code prettier and also added basic argument parsing. Included styleID source code. Added empty folder for styles.

--- dataset_downloader.py
+++ dataset_downloader.py
@@ -4,15 +4,15 @@ import pathlib
 import zipfile
 import glob
 from shutil import copy2, rmtree
+import subprocess
 
 
-if __name__ == "__main__":
-    cwd = pathlib.Path.cwd()
+def download_dataset(clean=False):
     temp_path = cwd / "tmp"
     pathlib.Path(temp_path).mkdir(parents=True, exist_ok=True)
 
     if not pathlib.Path(temp_path / "faces-20150624.zip").exists():
-        path, headers = urlretrieve("https://figshare.com/ndownloader/files/2134461", "tmp/faces-20150624.zip")
+        _, _ = urlretrieve("https://figshare.com/ndownloader/files/2134461", "tmp/faces-20150624.zip")
 
     if not pathlib.Path(temp_path / "data").exists():
         with zipfile.ZipFile(temp_path / "faces-20150624.zip", 'r') as zip_ref:
@@ -21,9 +21,52 @@ if __name__ == "__main__":
     face_dir = temp_path / "data" / "faces"
     classed_faces_dir = cwd / "data"
     pathlib.Path(classed_faces_dir).mkdir(parents=True, exist_ok=True)
+    counter = 0
     for filename in glob.glob(f"{face_dir}/*.jpg"):
-        if not (classed_faces_dir / pathlib.Path(filename).parts[-1]).exists():
-            copy2(filename, classed_faces_dir)
+        counter += 1
+        pathlib.Path(classed_faces_dir / str(counter)).mkdir(parents=True, exist_ok=True)
+        new_path = pathlib.Path(classed_faces_dir / str(counter) / pathlib.Path(filename).parts[-1])
+
+        if not new_path.exists():
+            copy2(filename, new_path)
+
+    if clean:
+        rmtree(temp_path)
+
+
+def apply_styles(folder):
+    return
+
+
+if __name__ == "__main__":
+    cwd = pathlib.Path.cwd()
+    download_flag = False
+    clean_flag = False
+    style_flag = False
+    style_folder = ""
 
     if "-c" in sys.argv or "--clean" in sys.argv:
-        rmtree(temp_path)
+        clean_flag = True
+    if len(sys.argv) > 1 and ("-d" in sys.argv or "--download" in sys.argv):
+        download_flag = True
+    if len(sys.argv) > 2 and ("-s" in sys.argv or "--style" in sys.argv):
+        if "-s" in sys.argv:
+            idx = sys.argv.index("-s")
+        else:
+            idx = sys.argv.index("--style")
+        style_flag = True
+        if pathlib.Path(cwd / sys.argv[idx+1]).exists():
+            style_folder = pathlib.Path(cwd / sys.argv[idx+1])
+        else:
+            print("Style folder not found.", file=sys.stderr)
+            exit(-1)
+
+    if not download_flag and not style_flag:
+        download_flag = True
+
+    if download_flag:
+        download_dataset(clean_flag)
+
+    if style_flag:
+        apply_styles(style_folder)
+
